ee1941b21ba6e53cd79789747b8e73f0e70aa637
O75wi <tit7tamtam@gmail.com>
2026-05-28 13:55:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+import telebot
+import os
 
+# سحب التوكن من المتغيرات في Railway
+TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "أهلاً بك! البوت يعمل الآن بشكل ممتاز.")
+
+# السطر الأهم: يجعل البوت يسمع الرسائل باستمرار
+bot.infinity_polling()
