e7f19caef91e57f6d42ab3e851ca15d2998b9ad7
O75wi <tit7tamtam@gmail.com>
2026-05-28 14:25:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
 import telebot
 import os
+from flask import Flask, send_from_directory
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
+import threading
 
 TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 bot = telebot.TeleBot(TOKEN)
+app = Flask(name)
+
+# مسار الموقع (سيرفر الويب)
+@app.route('/')
+def index():
+    return send_from_directory('.', 'index.html')
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     markup = InlineKeyboardMarkup()
-    # الرابط الصحيح الذي حصلت عليه
     web_app = WebAppInfo(url='https://telegramaibot-production-522e.up.railway.app') 
     btn = InlineKeyboardButton(text="START AI", web_app=web_app)
     markup.add(btn)
-    
-    bot.send_message(message.chat.id, "أهلاً بك! اضغط على الزر أدناه لبدء الشات مع الذكاء الاصطناعي:", reply_markup=markup)
+    bot.send_message(message.chat.id, "أهلاً بك! اضغط على الزر أدناه:", reply_markup=markup)
 
-bot.infinity_polling()
+# تشغيل البوت في خيط (Thread) منفصل
+def run_bot():
+    bot.infinity_polling()
+
+if name == 'main':
+    threading.Thread(target=run_bot).start()
+    app.run(host='0.0.0.0', port=8080)
