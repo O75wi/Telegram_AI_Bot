15de3b57bddb5b6d1c4058b3836c6c436051b524
O75wi <tit7tamtam@gmail.com>
2026-05-28 15:34:56 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 GEMINI_KEY = os.getenv('GEMINI_API_KEY')
 
 genai.configure(api_key=GEMINI_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-2.0-flash')
 
 bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
